2dbad1018b9ccb748cd0789cf0317b06c6458aeb
김기재 <110440120+kijae-kim@users.noreply.github.com>
2026-04-14 16:16:43 +0900

    [Bronze III] Title: 최소, 최대, Time: 308 ms, Memory: 132864 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,9 +1,9 @@
 import sys
 
 n = int(sys.stdin.readline())
-nums = list(map(int, sys.stdin.readline().split()))
+numbers = list(map(int,sys.stdin.readline().split()))
 
-min_value = min(nums)
-max_value = max(nums)
+min_value = min(numbers)
+max_value = max(numbers)
 
 print(min_value, max_value)
