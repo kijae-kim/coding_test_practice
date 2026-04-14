804798ae2d2e307b102d701886bab286d5f46fbb
김기재 <110440120+kijae-kim@users.noreply.github.com>
2026-04-14 17:05:20 +0900

    [Bronze II] Title: 음계, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,10 @@
+import sys
+
+scale = list(map(int, sys.stdin.readline().split()))
+
+if scale == [1,2,3,4,5,6,7,8]:
+    print("ascending")
+elif scale == [8,7,6,5,4,3,2,1]:
+    print("descending")
+else:
+    print("mixed")
